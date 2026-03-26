bf298260cb96d14441723a0592a8496838f20276
Maxim Mushinskiy <makmushinskiy@inbox.ru>
2026-03-26 20:30:15 +0300

    add crypo.helper in credentials.service

--- credential-bot/src/credentials/credentials.module.ts
+++ credential-bot/src/credentials/credentials.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CredentialsRepository } from './credentials.repository';
 import { CredentialsService } from './credentials.service';
+import { CryptoHelper } from '../bot/helpers/crypto.helper';
 
 @Module({
-  providers: [CredentialsRepository, CredentialsService],
-  exports: [CredentialsService],
+  providers: [CredentialsRepository, CredentialsService, CryptoHelper],
+  exports: [CredentialsService, CryptoHelper],
 })
 export class CredentialsModule {}
--- credential-bot/src/credentials/credentials.service.ts
+++ credential-bot/src/credentials/credentials.service.ts
@@ -1,36 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { CredentialsRepository } from './credentials.repository';
 import { CreateCredentialData, UpdateCredentialData } from './interfaces/credentials.interfaces';
+import { CryptoHelper } from '../bot/helpers/crypto.helper';
 
 @Injectable()
 export class CredentialsService {
-  constructor(private readonly credentialsRepository: CredentialsRepository) {}
+  constructor(
+    private readonly credentialsRepository: CredentialsRepository,
+    private readonly cryptoHelper: CryptoHelper,
+  ) {}
 
   create(userId: string, data: CreateCredentialData) {
-    return this.credentialsRepository.create(userId, data);
+    return this.credentialsRepository.create(userId, {
+      ...data,
+      password: this.cryptoHelper.encrypt(data.password),
+    });
   }
 
-  findAllByUser(userId: string) {
-    return this.credentialsRepository.findAllByUser(userId);
+  async findAllByUser(userId: string) {
+    const credentials = await this.credentialsRepository.findAllByUser(userId);
+    return credentials.map((c) => this.decryptCredential(c));
   }
 
-  findByGroup(userId: string, groupId: string) {
-    return this.credentialsRepository.findByGroup(userId, groupId);
+  async findByGroup(userId: string, groupId: string) {
+    const credentials = await this.credentialsRepository.findByGroup(userId, groupId);
+    return credentials.map((c) => this.decryptCredential(c));
   }
 
-  findWithoutGroup(userId: string) {
-    return this.credentialsRepository.findWithoutGroup(userId);
+  async findWithoutGroup(userId: string) {
+    const credentials = await this.credentialsRepository.findWithoutGroup(userId);
+    return credentials.map((c) => this.decryptCredential(c));
   }
 
-  findOne(credentialId: string, userId: string) {
-    return this.credentialsRepository.findOne(credentialId, userId);
+  async findOne(credentialId: string, userId: string) {
+    const credential = await this.credentialsRepository.findOne(credentialId, userId);
+    return credential ? this.decryptCredential(credential) : null;
   }
 
   update(credentialId: string, userId: string, data: UpdateCredentialData) {
-    return this.credentialsRepository.update(credentialId, userId, data);
+    const encrypted = data.password
+      ? { ...data, password: this.cryptoHelper.encrypt(data.password) }
+      : data;
+    return this.credentialsRepository.update(credentialId, userId, encrypted);
   }
 
   delete(credentialId: string, userId: string) {
     return this.credentialsRepository.delete(credentialId, userId);
+  }
+
+  private decryptCredential<T extends { password: string }>(credential: T): T {
+    return {
+      ...credential,
+      password: this.cryptoHelper.decrypt(credential.password),
+    };
   }
 }
